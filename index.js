cc93dfcf7d92a43d77cbedea2b11934dde0564f3
paul <noobie375@live.com>
2016-05-14 14:42:36 -0400

    Added the ability for the robot to talk using espeak

--- index.js
+++ index.js
@@ -3,47 +3,58 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
+const spawn = require('child_process').spawn;
+
+
+function say(saying) {
+    spawn('espeak', ['"' + saying + '"', '/usr']);
+}
+
 var port = new SerialPort('/dev/ttyACM0', {
-  parser: serialport.parsers.raw
+    parser: serialport.parsers.raw
 });
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-    if (msg == "FORWARD"){
-      port.write('W');
-    }
-    if (msg == "BACKWARD"){
-      port.write('S');
-    }
-    if (msg == "LEFT"){
-      port.write('A');
-    }
-    if (msg == "RIGHT"){
-      port.write('D');
-    }
-    if (msg == "STOP"){
-      port.write('Q');
-    }
+io.on('connection', function(socket) {
+    //console.log('a user connected');
+    socket.on('disconnect', function() {
+        //console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        if(msg.charAt(0) == 's'){
+          io.emit('chat message', "The Robot Said " + msg.substring(1, msg.length));
+        } else {
+          io.emit('chat message', "The Current State Of The Robot is " + msg);
+        }
+        switch (msg) {
+            case "FORWARD":
+                port.write('W');
+                break;
+            case "BACKWARD":
+                port.write('S');
+                break;
+            case "LEFT":
+                port.write('A');
+                break;
+            case "RIGHT":
+                port.write('D');
+                break;
+            case "STOP":
+                port.write('Q');
+                break;
+            case "SayJoke":
+                //console.log("SAY JOKE")
+                break;
+            default:
+                say(msg.substring(1, msg.length));
+                break;
+        }
 
-  });
+    });
 });
 
-/*io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});*/
-
-
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(8000, function() {
+    console.log('listening on *:8000');
 });
