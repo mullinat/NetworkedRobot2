37cb9afa0a50d6b833c1cd319d43a1c3a8d8b8c1
paul <noobie375@live.com>
2016-05-14 15:32:31 -0400

    Reading Jokes Now Works With Custom Sayings Now Works Reading is done with the command line espeak, say, or festival

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var Questions = JSON.parse(fs.readFileSync('Questions.json', 'utf8'));
+var Answers = JSON.parse(fs.readFileSync('Answers.json', 'utf8'));
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -5,9 +8,19 @@ var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 const spawn = require('child_process').spawn;
 
+function readJoke(saying) {
+    spawn('espeak', ['"' + saying + '"', '/usr'], function(err, data) {
+        if (err) return console.error(err);
+        console.log(data.toString());
+    });
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
 
 function say(saying) {
-    spawn('espeak', ['"' + saying + '"', '/usr']);
+    spawn('say', ['"' + saying + '"', '']);
 }
 
 var port = new SerialPort('/dev/ttyACM0', {
@@ -23,10 +36,10 @@ io.on('connection', function(socket) {
     });
     socket.on('chat message', function(msg) {
         console.log('message: ' + msg);
-        if(msg.charAt(0) == 's'){
-          io.emit('chat message', "The Robot Said " + msg.substring(1, msg.length));
+        if (msg.charAt(0) == 's') {
+            io.emit('chat message', "The Robot Said " + msg.substring(1, msg.length));
         } else {
-          io.emit('chat message', "The Current State Of The Robot is " + msg);
+            io.emit('chat message', "The Current State Of The Robot is " + msg);
         }
         switch (msg) {
             case "FORWARD":
@@ -45,7 +58,11 @@ io.on('connection', function(socket) {
                 port.write('Q');
                 break;
             case "SayJoke":
-                //console.log("SAY JOKE")
+                jokeNum = randomInt(0, Questions.length);
+                say(Questions[jokeNum]);
+                break;
+            case "SayPunchLine":
+                say(Answers[jokeNum]);
                 break;
             default:
                 say(msg.substring(1, msg.length));
@@ -54,6 +71,8 @@ io.on('connection', function(socket) {
 
     });
 });
+
+
 
 http.listen(8000, function() {
     console.log('listening on *:8000');
